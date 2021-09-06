161357f64f97bfaca706e70c1b0bf9e1aa8561e9
Sdsdsde2 <imspammails31@gmail.com>
2021-09-05 23:46:22 -0500

    added examples of the Java variable syntax.

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World");
+	int varInt = 7;               // integer (whole number)
+    float varFloat = 1.99f;    // floating point number
+    char varChar = 'S';         // character
+    boolean varBool = true;       // boolean
+    String varString = "Hello";     // String    
   }
 }
